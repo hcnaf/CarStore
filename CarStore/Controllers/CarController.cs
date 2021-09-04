6a4c72714dca9a7ee3c3ddd17720b43a4d1b4aca
Ань Туан Фан Чан <fanchana@yandex.ru>
2021-09-04 17:29:21 +0300

    Add project files.

---
+++ CarStore/Controllers/CarController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarStore.Models;
+
+namespace CarStore.Controllers
+{
+    public class CarController : Controller
+    {
+        /// <summary>
+        /// Car repository
+        /// </summary>
+        private ICarRepository repository;
+
+        /// <summary>
+        /// Car controller ctor.
+        /// </summary>
+        /// <param name="repository"></param>
+        public CarController(ICarRepository repository)
+        {
+            this.repository = repository ?? throw new ArgumentNullException();
+        }
+
+        public ViewResult List() => View(repository.Products);
+    }
+}
---
+++ CarStore/Models/CarsDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarStore.Models
+{
+    public class CarsDbContext : DbContext
+    {
+        public CarsDbContext(DbContextOptions<CarsDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;DataBase=CarStore;Trusted_Connection=True;MultipleActiveReusltSets=true");
+            }
+        }
+
+        public DbSet<Car> Cars { get; set; }
+    }
+}
