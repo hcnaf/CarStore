3c4d4ce8018e14c32ed60376417213ee044c8c79
Ань Туан Фан Чан <fanchana@yandex.ru>
2021-09-17 20:48:01 +0300

    Cart widget

---
+++ CarStore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using CarStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CarStore.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+        public CartSummaryViewComponent(Cart cart)
+        {
+            this.cart = cart ?? throw new ArgumentNullException(nameof(cart));
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(this.cart);
+        }
+    }
+}
