2d7a4da974e0b0a2436bf96e9ee039cd141d4b93
Ань Туан Фан Чан <fanchana@yandex.ru>
2021-09-17 23:30:33 +0300

    Cart checkout

---
+++ CarStore/Controllers/OrderController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CarStore.Models;
+
+namespace CarStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ViewResult Checkout() => View(new Order());
+    }
+}
---
+++ CarStore/Models/Order.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace CarStore.Models
+{
+    public class Order
+    {
+        [BindNever]
+        public int OrderId { get; set; }
+
+        [BindNever]
+        public ICollection<CartLine> Lines { get; set; }
+
+        [Required(ErrorMessage = "Please enter your name.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your country.")]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "Please enter your city.")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Please enter your address.")]
+        public string Address { get; set; }
+
+        public string PostCode { get; set; }
+
+        public bool GiftWrap { get; set; }
+    }
+}
