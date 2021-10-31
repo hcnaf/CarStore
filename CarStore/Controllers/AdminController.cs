b84b72098d6e4af1b1686d4df1cd1104af6aeabf
Anh Tuan Phan Tran <Anh_Tuan_Phan_Tran@epam.com>
2021-10-31 18:39:05 +0300

    Login, Logout - NWY p.340

--- CarStore/Controllers/AdminController.cs
+++ CarStore/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using CarStore.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace CarStore.Controllers
 {
+    [Authorize]
     public class AdminController : Controller
     {
         ICarRepository repository;
---
+++ CarStore/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarStore.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [UIHint("password")]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; } = "/";
+    }
+}
